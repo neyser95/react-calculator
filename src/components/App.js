9025ce8047bcfb8703ca1b0abf6d4fe8aaf858f3
neyser zana <neyserj.zana@gmail.com>
2019-02-17 23:15:49 -0500

    creating state and button layout component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,112 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super(),
-    this.state = {
-
-    }
+      (this.state = {
+        userInput: '',
+        buttons: [
+          {
+            value: 'AC',
+            btnClass: 'grey-btn',
+            btnNumber: 27
+          },
+          {
+            value: '±',
+            btnClass: 'grey-btn',
+            btnNumber: 16
+          },
+          {
+            value: '%',
+            btnClass: 'grey-btn',
+            btnNumber: 53
+          },
+          {
+            value: '÷',
+            btnClass: 'orange-btn',
+            btnNumber: 191
+          },
+          {
+            value: 7,
+            btnClass: 'light-grey-btn',
+            btnNumber: 55
+          },
+          {
+            value: 8,
+            btnClass: 'light-grey-btn',
+            btnNumber: 56
+          },
+          {
+            value: 9,
+            btnClass: 'light-grey-btn',
+            btnNumber: 57
+          },
+          {
+            value: 'x',
+            btnClass: 'orange-btn',
+            btnNumber: 56
+          },
+          {
+            value: 4,
+            btnClass: 'light-grey-btn',
+            btnNumber: 55
+          },
+          {
+            value: 5,
+            btnClass: 'light-grey-btn',
+            btnNumber: 56
+          },
+          {
+            value: 6,
+            btnClass: 'light-grey-btn',
+            btnNumber: 57
+          },
+          {
+            value: '-',
+            btnClass: 'orange-btn',
+            btnNumber: 56
+          },
+          {
+            value: 1,
+            btnClass: 'light-grey-btn',
+            btnNumber: 55
+          },
+          {
+            value: 2,
+            btnClass: 'light-grey-btn',
+            btnNumber: 56
+          },
+          {
+            value: 3,
+            btnClass: 'light-grey-btn',
+            btnNumber: 57
+          },
+          {
+            value: '+',
+            btnClass: 'orange-btn',
+            btnNumber: 56
+          },
+          {
+            value: 0,
+            btnClass: 'zero-grey-btn',
+            btnNumber: 55
+          },
+          {
+            value: '.',
+            btnClass: 'light-grey-btn',
+            btnNumber: 56
+          },
+          {
+            value: '=',
+            btnClass: 'orange-btn',
+            btnNumber: 56
+          }
+        ]
+      });
   }
 
-  render(){
-    return(
-      <h1 id='title'>Hello!!</h1>
-    );
+  render() {
+    return <h1 id='title'>Hello!!</h1>;
   }
 }
 
